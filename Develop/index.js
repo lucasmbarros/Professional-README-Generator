70f8a4fcd499c7364bd5a2b07c520c7947d8d868
Lucas Barros <lucasmello.barros@gmail.com>
2020-10-05 00:12:17 -0400

    add code, refactor, grammer errors, markdown changes

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,146 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkDown = require("./utils/generateMarkdown");
 
-];
+const questions = () => {
+  console.log(`
+    =================
+    Generate ReadMe!
+    =================
+    `);
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "title",
+      message: "What is your project title? (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter project title!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Enter your project's description (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your project's description!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "install",
+      message:
+        "What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running. (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log(
+            "Please provide instructions on how to install your project!"
+          );
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "usage",
+      message:
+        "Provide instructions and examples for use. Include screenshots if/as needed. (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please state uses for this project!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "input",
+      name: "credits",
+      message: "List collaborators to this project",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please include collaborators!");
+          return false;
+        }
+      },
+    },
+    {
+      type: "checkbox",
+      name: "license",
+      message: "Please select a license from the list below.",
+      choices: [
+        {
+          value: "Apache License 2.0",
+        },
+        {
+          value: "GNU General Public License v3.0",
+        },
+        {
+          value: "MIT License",
+        },
+        {
+          value: "ISC License",
+        },
+      ],
+    },
+    {
+      type: "input",
+      name: "user",
+      message: "Enter your github username (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please type in your github username !");
+          return false;
+        }
+      },
+    },
 
-// function to write README file
+    {
+      type: "input",
+      name: "email",
+      message: "Enter your email address (Required)",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please type in your email address !");
+          return false;
+        }
+      },
+    },
+  ]);
+};
+questions().then((answers) => {
+  console.log(answers);
+
+  const template = generateMarkDown(answers);
+  writeToFile("generatedReadMe.md", template);
+});
+
 function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, function (err) {
+    if (err) {
+      return console.log(err);
+    }
+    return console.log("File generated!");
+  });
 }
 
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
+function init() {}
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,27 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `
+  # ${data.title}
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Credits](#credits)
+  * [License](#license)
+  * [Questions](#questions)
+  ## Installation
+  ${data.install}
+  ## Usage
+  ${data.usage}
+  ## Credits
+  Made with ❤️ by: ${data.credits}
+  ## License
+  ${data.license}
+  ## Questions
+  [Github](https://github.com/${data.user})
+  ${data.email}
+  
 `;
 }
 
